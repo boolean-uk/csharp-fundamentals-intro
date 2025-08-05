a4923f64cd5978b3100642215e66afd996e72479
uerbzr <nigel.sibbert@gmail.com>
2025-08-05 13:27:56 +0100

    first commit

---
+++ workshop.console/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop.console
+{
+    /// <summary>
+    /// A class blueprint for a car in our application
+    /// </summary>
+    public class Car
+    {
+        private string _make;
+
+        private string _model;
+
+        public string Make { get { return _make; } set { _make = value; } }
+        public string Model { get => _model; set => _model = value; }
+        public string Description { get { return $"{_make} {_model}"; } } 
+        public int WheelCount = 0;
+    }
+}
---
+++ workshop.console/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using workshop.console;
+using workshop.console.Animals;
+
+Car beetle = new() { Make="VW", Model = "Beetle", WheelCount=4 };
+Car mini = new Car();
+mini.Make = "Mini";
+mini.Model = "Clubman";
+mini.WheelCount = 4;
+
+
+List<Car> cars = new List<Car>();
+
+cars.Add(mini);
+cars.Add(beetle);
+
+
+foreach(Car car in cars)
+{
+    Console.WriteLine($"Make:{car.Make} | Model: {car.Model} | Wheels: {car.WheelCount}");
+    Console.WriteLine($"Description:{car.Description}");
+}
+
+
+//Cat cat = new Cat() { Name="Red" };
+
+Console.WriteLine();
